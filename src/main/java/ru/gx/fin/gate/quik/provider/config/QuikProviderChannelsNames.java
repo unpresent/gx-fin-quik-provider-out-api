b5592af36120bd682cdc54ab2cded90225e1fbc7
Vladimir Gagarkin <vladimir.gagarkin@gmail.com>
2021-12-08 23:30:08 +0300

    v1.2.6. ChannelsNames -> QuikProviderChannelsNames.

--- src/main/java/ru/gx/fin/gate/quik/provider/config/QuikProviderChannelsNames.java
+++ src/main/java/ru/gx/fin/gate/quik/provider/config/QuikProviderChannelsNames.java
@@ -1,15 +1,15 @@
 package ru.gx.fin.gate.quik.provider.config;
 
 @SuppressWarnings("unused")
-public abstract class ChannelsNames {
-    public abstract static class QuikProviderStreams {
+public abstract class QuikProviderChannelsNames {
+    public abstract static class Streams {
         public static final String ALL_TRADES = "quik-provider-streams.all-trades";
         public static final String ORDERS = "quik-provider-streams.orders";
         public static final String DEALS = "quik-provider-streams.deals";
         public static final String SECURITIES = "quik-provider-streams.securities";
     }
 
-    public abstract static class QuikProviderSnapshots {
+    public abstract static class Snapshots {
         public static final String SECURITIES = "quik-provider-snapshots.securities";
     }
 }
